21e32f4107ca5685eae1747d229ff5d2602c83e1
wahaj-hafeez <wahaj.hafeez@codedistrict.com>
2024-01-29 19:37:44 +0500

    manage utils files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("errror", (error) => {
+      console.log("ERRR: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is runing at port : ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!!", err);
+  });
 
 /*
 import express from "express";
